60c6e2dcc20ce2ecb6ecb75182015061e392e6ec
estherlainez <esthertarazona81@gmail.com>
2019-10-09 23:11:28 +0200

    ...

--- 2DAM_AD_UD01_Practica1_EstherLainez/src/practica2/Ejercicio1.java
+++ 2DAM_AD_UD01_Practica1_EstherLainez/src/practica2/Ejercicio1.java
@@ -1,20 +1,11 @@
 package practica2;
-import java.io.DataOutputStream;
-import java.io.EOFException;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutput;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.util.*;
 public class Ejercicio1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
 		Scanner teclado=new Scanner(System.in);
-
 		
 		System.out.println("Introduzca el DNI: ");
 		String dni=teclado.nextLine();
@@ -27,55 +18,60 @@ public class Ejercicio1 {
 		System.out.println("Introduzca el telefono: ");
 		String telefono=teclado.nextLine();
 		
-		DatosPersona dp=new DatosPersona(dni,nombre,apellido,fecha,telefono);
-		System.out.println(dp);
-		
-
-		String ruta="src/";	
-		String nombreF= "datos.txt";
-		File f= new File(ruta+nombre);
+		String ruta="src/";
+		String nombreF="datosPersona.txt";
+		File f=new File(ruta+nombreF);
 		
 		try {
 			f.createNewFile();
-			FileOutputStream fileout = new FileOutputStream(f);
-			ObjectOutputStream objectout = new ObjectOutputStream(fileout);
-			
-			objectout.writeObject(dp);
-			
-			objectout.close();
-		}catch(IOException e) {
-			e.printStackTrace();
-		}
-
-		int i=0;
-		
-		FileInputStream filein=null;
-		ObjectInputStream objectin=null;
-		try {
-			filein=new FileInputStream(f);
-			objectin= new ObjectInputStream(filein);
-			
-			while(true) {
-				dp=(DatosPersona) objectin.readObject();
+			FileWriter fw=new FileWriter(f);
+			BufferedWriter bw=new BufferedWriter(fw);
+			/*
+			String []datos= {dni,nombre,apellido,fecha,telefono};
+			for(int i=0;i<datos.length;i++) {
+				bw.write(datos[i]);
+				bw.newLine();
 				
 			}
+			*/
+			bw.write(dni);
+			bw.write("|");
+			bw.write(nombre);
+			bw.write("|");
+			bw.write(apellido);
+			bw.write("|");
+			bw.write(fecha);
+			bw.write("|");
+			bw.write(telefono);
+			bw.write("|");
+			
+			bw.close();
+			fw.close();
 			
 		}catch(IOException e) {
 			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-		
-			e.printStackTrace();
-		}
-		finally {
-			try {
-				objectin.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}	
 		}
 		
-		
-		
+		try {
+			FileReader fr=new FileReader(f);
+			BufferedReader br=new BufferedReader(fr);
+			//String datos="";
+			String linea="";
+			String []campos=linea.split("\\|");
+			while((linea=br.readLine())!=null){
+				System.out.println("sus datos "+linea);
+				//system.nombre + campos[0.....
+			}
+			
+			br.close();
+			fr.close();
+			
+			
+		}catch(IOException e){
+			e.getStackTrace();
+			
+		}
+
 	}
 
 }
